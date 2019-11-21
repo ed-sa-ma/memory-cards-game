d9967a3afbc0d8012627195101c71f139295adb7
Eduardo Sanz Martin <>
2019-11-21 21:11:03 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import Board from "./components/Board";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div className="headlineContainer">
+        <h1 className="headline"> MEMORY CARDS </h1>
+      </div>
       <Board />
     </div>
   );
