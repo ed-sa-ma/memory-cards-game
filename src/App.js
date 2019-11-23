88412c7c5e806aff3869a07794e8f48de85d4147
Eduardo Sanz Martin <>
2019-11-23 13:47:16 +0100

    create game counter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import Board from "./components/Board";
 import "./App.css";
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import useRandomColors from "../hooks/useRandomColors";
 import useCardsState from "../hooks/useCardsState";
@@ -13,20 +13,38 @@ function Board({ numberOfCards = 20 }) {
   const randomColors = useRandomColors(POSSIBLE_COLORS, numberOfCards);
   const [cardsState, flipCard] = useCardsState(randomColors);
 
+  const [counter, setCounter] = useState(0);
+
+  function handleClickCard(idx) {
+    if(!cardsState[idx].visible) {
+      setCounter(prevCounter => prevCounter + 1);
+    }
+
+    flipCard(idx);
+  }
+
   return (
-    <div className="board">
-      {
-        cardsState.map(({ color, visible, present }, idx) => 
-          <Card
-            color={color}
-            key={idx}
-            onClick={() => flipCard(idx)}
-            present={present}
-            visible={visible}
-          />
-        )
-      }
-    </div>
+    <>
+      <div className="board">
+        {
+          cardsState.map(({ color, visible, present }, idx) => 
+            <Card
+              color={color}
+              key={idx}
+              onClick={() => handleClickCard(idx)}
+              present={present}
+              visible={visible}
+            />
+          )
+        }
+      </div>
+      <div className="counter">
+        <div className="counter--badge">
+          { `Moves: ${counter} *` }
+        </div>
+        <span className="counter--note">{`*Perfect game: ${randomColors.length / 2}`}</span>
+      </div>
+    </>
   );
 };
 
