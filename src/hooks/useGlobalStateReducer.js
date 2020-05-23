77625e9c435d3f49651a5f558ba6e20a438f6ec7
Eduardo Sanz Martin <>
2020-05-23 18:51:44 +0200

    Change color palette for cards

--- src/hooks/useGlobalStateReducer.js
+++ src/hooks/useGlobalStateReducer.js
@@ -2,16 +2,18 @@ import { useReducer, useEffect } from "react";
 import useRandomColors from "./useRandomColors";
 
 const NUMBER_OF_CARDS = 16;
+// Color palette generated here:
+// https://coolors.co/063f50-724e56-db504a-df8328-e3b505-9b9f4b-56a3a6-e6e1c5-ada375
 const POSSIBLE_COLORS = [
-  "red",
-  "blue",
-  "yellow",
-  "grey",
-  "brown",
-  "aqua",
-  "green",
-  "yellowgreen",
-  "violet",
+  "#063F50",
+  "#724E56",
+  "#DB504A",
+  "#DF8328",
+  "#E3B505",
+  "#9B9F4B",
+  "#56A3A6",
+  "#E6E1C5",
+  "#ADA375",
 ];
 
 function reducer(state, action) {
